a76254800ec819aea4a815db7cf5d67f04ea038b
DanDeLuna <danieldeluna4@gmail.com>
2023-07-13 19:00:18 -0600

    Contact Page

---
+++ src/componets/pages/contact.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+
+import { checkEmail } from "../../ulti/help";
+
+const styles = {
+  h2: {
+    textAlign: "center",
+  },
+  form: {
+    textAlign: "center",
+  },
+  email: {
+    textAlign: "center",
+    color: "#0000FF",
+  },
+  input: {
+    margin: "0.5rem",
+  },
+  inputb: {
+    height: "4rem",
+  },
+};
+
+function Form() {
+  const [email, setEmail] = useState("");
+  const [Name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "Name") {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!checkEmail(email)) {
+      alert("Email is Invalid");
+      return;
+    }
+
+    if (!Name) {
+      alert("Required");
+      return;
+    }
+
+    if (!message) {
+      alert("Required");
+      return;
+    }
+
+    if (email || Name || message) {
+      setSuccessMessage("Success");
+      return;
+    }
+
+    setName("");
+    setMessage("");
+    setEmail("");
+  };
+
+  return (
+    <div>
+      <h2 style={styles.h2}>Contact</h2>
+      <div style={styles.email}>
+        <a href="mailto:danieldeluna4@gmail.com">danieldeluna4@gmail.com</a>
+      </div>
+      <form action="mailto:danieldeluna4@gmail.com.com" method= "post" encType="text/plain" style={styles.form} className="form">
+        <div style={styles.input}>
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="email"
+            required
+          />
+        </div>
+        <div style={styles.input}>
+          <input
+            value={Name}
+            name="Name"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="name"
+            required
+          />
+        </div>
+        <div style={styles.input}>
+          <input
+            style={styles.inputb}
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="message"
+            placeholder="message"
+            required
+          />
+        </div>
+        <div>
+          <button type="button" onClick={handleFormSubmit}>
+            Submit
+          </button>
+        </div>
+      </form>
+      {successMessage && (
+        <div style={styles.form}>
+          <p className="success-text">{successMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Form;
